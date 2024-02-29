322b3074acc0b99fa36ea201f37ec23ae03503e6
omprakash9199 <161561124+omprakash9199@users.noreply.github.com>
2024-02-29 13:41:15 +0530

    toast
    this is add main activity in androaid

--- app/src/main/java/com/example/toast/MainActivity.kt
+++ app/src/main/java/com/example/toast/MainActivity.kt
@@ -2,12 +2,27 @@ package com.example.toast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button1:Button
+    lateinit var button2:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button1 = findViewById(R.id.button1)
+        button2 = findViewById(R.id.button2)
+
+        button1.setOnClickListener {
+            Toast.makeText(this@MainActivity, "this is short", Toast.LENGTH_SHORT).show()
+        }
+        button2.setOnClickListener {
+            Toast.makeText(this@MainActivity, "this is long  short", Toast.LENGTH_SHORT).show()
+        }
 
     }
+
+
 }
